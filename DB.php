330a3765427ac3578f813a1fad109808fd6b92c6
Sebastian Steininger <basti@batchcloud.local>
2020-12-22 11:52:36 +0100

    edit public

---
+++ DB.php
@@ -0,0 +1,24 @@
+<?php
+        class DB {
+
+                private $pdo;
+
+                public function __construct($host, $dbname, $username, $password) {
+                                        $pdo = new PDO('mysql:host=localhost;dbname=test;charset=utf8', '', '');
+                        
+                        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                        $this->pdo = $pdo;
+                }
+
+                public function query($query, $params = array()) {
+                        $statement = $this->pdo->prepare($query);
+                        $statement->execute($params);
+
+                        if (explode(' ', $query)[0] == 'SELECT') {
+                        $data = $statement->fetchAll();
+                        return $data;
+                        }
+                }
+
+        }
+?>
