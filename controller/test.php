1b4060f88ccb05ad3fc70da59ddd862f01af02c9
Marvin Lukaschek <Groruk@googlemail.com>
2020-12-22 12:38:15 +0100

    Add TestController

---
+++ controller/test.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace HWS\Controller;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response;
+
+class TestController
+{
+    /**
+     * @var \DB
+     */
+    protected $db;
+
+    public function __construct(DB $db)
+    {
+        $this->db = $db;
+    }
+
+    public function __invoke(ServerRequestInterface $request) : ResponseInterface
+    {
+        $testdata = $db->query('SELECT name FROM test');
+
+        $response->getBody()->write($testdata);
+        return $response->withStatus(200);
+    }
+}
