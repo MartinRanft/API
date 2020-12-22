515d44ae5ef173b629560667da961197c395d7c1
Sebastian Steininger <basti@batchcloud.local>
2020-12-22 08:45:51 +0100

    init

--- index.php
+++ index.php
@@ -1,7 +1,49 @@
 <?php
-include_once '../config/database.php';
+require 'vendor/autoload.php';
+//include_once '../config/database.php';
+
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+$request = Laminas\Diactoros\ServerRequestFactory::fromGlobals(
+    $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
+);
+
+$responseFactory = new \Laminas\Diactoros\ResponseFactory();
+
+$strategy = new League\Route\Strategy\JsonStrategy($responseFactory);
+$router   = (new League\Route\Router)->setStrategy($strategy);
+
+
+
+// map a route
+$router->map('GET', '/', function (ServerRequestInterface $request) : array {
+    
+    
+
+    
+    return [
+        'title'   => 'My New Simple API',
+        'version' => 1,
+    ];
+});
+
+
+
+$response = $router->dispatch($request);
+
+// send the response to the browser
+(new Laminas\HttpHandlerRunner\Emitter\SapiEmitter)->emit($response);
+
+
+
+
+
+
 
 $database = new Database();
+
 $db = $database->getConnection();
 
 
@@ -9,8 +51,7 @@ if ($_SERVER['REQUEST_METHOD'] == "GET") {
 
     if ($_GET['url'] == "student") {
         
-sdsdsds
-
+        
 
     }   
     // get schüler
